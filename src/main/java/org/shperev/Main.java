d90e8e08db95b21c44ed454324c18a1df9799b8a
lmanohara <lmanohara99@gmail.com>
2024-10-25 10:55:43 +0100

    Add initial websocket handshake implementation

---
+++ src/main/java/org/shperev/Main.java
@@ -0,0 +1,17 @@
+package org.shperev;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        WebsocketServer websocketServer = new WebsocketServer();
+
+            ServerSocket serverSocket = websocketServer.initiateSocketServer();
+        while (true){
+            websocketServer.accept(serverSocket);
+        }
+
+    }
+}
---
+++ src/main/java/org/shperev/WebsocketConnection.java
@@ -0,0 +1,4 @@
+package org.shperev;
+
+public class WebsocketConnection {
+}
